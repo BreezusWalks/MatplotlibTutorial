b97e7305cb4f7065b7d25bbf22f48658c0d2647b
nz6j8k <kellen.miller@gm.com>
2019-01-28 15:27:37 -0600

    Finishing Part 10 Finishing Part 11

--- 11_UnixTime.py
+++ 11_UnixTime.py
@@ -1,14 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import urllib
-import matplotlib.dates as mdates
-
-def bytespdate2num(fmt, encoding='utf-8'):
-    strconverter = mdates.strpdate2num(fmt)
-    def bytesconverter(b):
-        s = b.decode(encoding)
-        return strconverter(s)
-    return bytesconverter
+import datetime as dt
 
 def graph_data():
     stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
@@ -23,23 +16,18 @@ def graph_data():
             if 'values' not in line:
                 stock_data.append(line)
 
-    # %Y = full year. 2015
-    # %y = partial year 15
-    # %m = number month
-    # %d = number day
-    # %H = hours
-    # %M = minutes
-    # %S = seconds
-    # 12-06-2014
-    # %m-%d-%Y
-    date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data, delimiter=',', unpack=True, converters={0: bytespdate2num('%Y-%m-%d')})
+    date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data, delimiter=',', unpack=True)
+
+    #UNIX TIME CONVERSION
+    dateconv = np.vectorize(dt.datetime.fromtimestamp)
+    date = dateconv(date)
 
     fig = plt.figure()
     ax1 = plt.subplot2grid((1,1), (0,0))
     ax1.plot_date(date, closep, '-', label='Price')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
-    ax1.grid(True)
+    ax1.grid(True, color='g', linestyle='-', linewidth=5)
 
     plt.xlabel('Date')
     plt.ylabel('Price')
@@ -48,4 +36,6 @@ def graph_data():
     plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94, top=0.90, wspace=0.2, hspace=0)
     plt.show()
 
-graph_data()
+#graph_data()
+print("Function does not work since the time stamps being inputted from the hardcoded page are in Y-M-D format. "
+      "Did not feel like setting up a quandl.get for the stock and finding it in unix time.")
